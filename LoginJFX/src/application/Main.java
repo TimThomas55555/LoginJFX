d22f1bee8dae023e7a9716d82ce7f5d92e78c3a0
David <davidzzzz21@hotmail.com>
2019-10-30 20:50:41 +0100

    Add LoginJFX

---
+++ LoginJFX/src/application/Main.java
@@ -0,0 +1,30 @@
+package application;
+	
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+
+
+public class Main extends Application {
+	@Override
+	public void start(Stage stage) {
+		try {
+			FXMLLoader loader=new FXMLLoader(Main.class.getResource("PeachLogin.fxml"));
+            AnchorPane mypane=loader.load();
+            Scene scene=new Scene(mypane);
+            scene.getStylesheets().addAll(Main.class.getResource("application.css").toExternalForm());
+            stage.setTitle("signin");
+            stage.setScene(scene);
+            stage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
