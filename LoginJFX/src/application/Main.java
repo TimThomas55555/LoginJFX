50e157c21209806da8b3dc3a94bd6c3c3eb497af
David <davidzzzz21@hotmail.com>
2019-11-12 19:46:54 +0100

    tercera rama

--- LoginJFX/src/application/Main.java
+++ LoginJFX/src/application/Main.java
@@ -1,31 +1,56 @@
 package application;
 	
 import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 
 
 public class Main extends Application {
+	Stage stage;
+
+	@FXML
+	Label logoLabel;
+
 	@Override
-	public void start(Stage stage) {
+	public void start(Stage myStage) throws Exception {
+
+		stage = myStage;
+
+		VentanaPrincipal();
+
+	}
+
+	private void VentanaPrincipal() {
+
 		try {
-			FXMLLoader loader=new FXMLLoader(Main.class.getResource("PeachLogin.fxml"));
-            AnchorPane mypane=loader.load();
-            Scene scene=new Scene(mypane);
-            scene.getStylesheets().addAll(Main.class.getResource("application.css").toExternalForm());
-            stage.setTitle("LOGIN");
-            stage.initStyle(StageStyle.UNDECORATED);
-            stage.setScene(scene);
-            stage.show();
-		} catch(Exception e) {
+
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("Splash.fxml"));
+			AnchorPane myPane = loader.load();
+			Scene scene = new Scene(myPane);			
+
+			// adding Google fonts
+			//scene.getStylesheets().add("https://fonts.googleapis.com/css?family=McLaren");
+			
+			stage.initStyle(StageStyle.UNDECORATED);
+			stage.setScene(scene);
+			stage.show();
+
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
+
 	}
-	
+
+	public void closeStage() {
+		stage.close();
+	}
+
 	public static void main(String[] args) {
 		launch(args);
 	}
