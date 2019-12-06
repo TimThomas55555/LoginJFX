85bfc722f48275cd7bad1bc5e0e40fe65bb8c6c4
David <davidzzzz21@hotmail.com>
2019-12-06 21:02:38 +0100

    change login

--- LoginJFX/src/application/Main.java
+++ LoginJFX/src/application/Main.java
@@ -9,8 +9,14 @@ import javafx.stage.StageStyle;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
-
-
+/**
+ * Aplicacion de JavaFX que se inicia con un Splash, seguida de un Wizard con una breve ayuda al usuario, continuando con un Login, para dar paso a un Homepage
+ * @author david
+ * @see ControladorSplash
+ * @see ControladorWizard
+ * @see Controlador
+ * @see Controlador_homapage
+ */
 public class Main extends Application {
 	Stage stage;
 
